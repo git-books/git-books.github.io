cff0f77ee928613250caef0390aea2326ac649f2
inhere <in.798@qq.com>
2017-09-18 17:42:52 +0800

    add new book: JS 函数式编程指南中文版

---
+++ books/mostly-adequate-guide-chinese/config.js
@@ -0,0 +1,26 @@
+  // page title = baseTile + content title
+config.baseTitle = 'JS 函数式编程指南中文版'
+config.siteName = 'JS 函数式编程指南中文版'
+config.siteDes = 'JS 函数式编程指南中文版'
+config.siteKey = 'mostly-adequate-guide-chinese'
+config.logoUrl = '/books/mostly-adequate-guide-chinese/'
+config.assetBasePath = '/'
+
+config.docProject = 'llh911001/mostly-adequate-guide-chinese'
+config.dataUrl = 'https://raw.githubusercontent.com/llh911001/mostly-adequate-guide-chinese/master/'
+config.docUrl = 'https://github.com/llh911001/mostly-adequate-guide-chinese'
+config.editUrl = 'https://github.com/llh911001/mostly-adequate-guide-chinese/blob/master'
+
+config.project = 'llh911001/mostly-adequate-guide-chinese'
+config.projectUrl = 'https://github.com/llh911001/mostly-adequate-guide-chinese'
+config.issueUrl = 'https://github.com/llh911001/mostly-adequate-guide-chinese/issues'
+
+config.authorPage = 'https://github.com/llh911001'
+config.authorName = 'llh911001'
+
+config.theme = 'paper'
+config.codeTheme = 'github'
+config.catelogPage = 'SUMMARY.md'
+config.defaultPage = 'README.md'
+config.makeTOC = true
+config.emptyData = 'No content to display!'
