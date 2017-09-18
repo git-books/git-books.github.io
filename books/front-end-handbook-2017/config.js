55d8cf1040fa266ebb552ebdf6f36f19f815039e
inhere <in.798@qq.com>
2017-09-18 14:37:25 +0800

    bug fixed

--- books/front-end-handbook-2017/config.js
+++ books/front-end-handbook-2017/config.js
@@ -1,7 +1,7 @@
   // page title = baseTile + content title
 config.baseTitle = 'Front-End Developer Handbook 2017'
 config.siteName = 'Front-End Developer Handbook 2017'
-config.siteDes = 'This is a guide that anyone could use to learn about the practice of front-end development.'
+// config.siteDes = 'This is a guide that anyone could use to learn about the practice of front-end development.'
 config.siteKey = 'front-end-handbook-2017'
 config.logoUrl = '/books/front-end-handbook-2017/'
 config.assetBasePath = '/'
