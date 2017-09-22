955f492e01f62dd8a4c3bd0faae77025bc71959c
inhere <in.798@qq.com>
2017-09-22 10:57:25 +0800

    add new book

---
+++ books/dubbo/config.js
@@ -0,0 +1,27 @@
+  // page title = baseTile + content title
+config.baseTitle = 'alibaba java dubbo'
+config.siteName = 'alibaba java dubbo'
+config.siteDes = '详细讲解了dubbo的使用，基本涵盖dubbo的所有功能特性'
+config.siteKey = 'alibaba-java-dubbo'
+config.logoUrl = '/books/dubbo/'
+config.assetBasePath = '/'
+
+config.docProject = 'dubbo/dubbo-user-book'
+config.dataUrl = 'https://raw.githubusercontent.com/dubbo/dubbo-user-book/master/'
+config.docUrl = 'https://github.com/dubbo/dubbo-user-book'
+config.editUrl = 'https://github.com/dubbo/dubbo-user-book/blob/master'
+config.docIssueUrl = 'https://github.com/dubbo/dubbo-user-book/issues'
+
+config.project = 'alibaba/dubbo'
+config.projectUrl = 'https://github.com/alibaba/dubbo'
+config.issueUrl = 'https://github.com/alibaba/dubbo/issues'
+
+config.authorPage = 'https://github.com/alibaba'
+config.authorName = 'alibaba'
+
+config.theme = 'paper'
+config.codeTheme = 'github'
+config.catelogPage = 'SUMMARY.md'
+config.defaultPage = 'README.md'
+config.makeTOC = true
+config.emptyData = 'No content to display!'
