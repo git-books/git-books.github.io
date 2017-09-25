0435b6d7cfb89b4eca03e8de0347829c29f22d17
Inhere <inhere@users.noreply.github.com>
2017-09-25 16:25:49 +0800

    Create config.js

---
+++ books/php-co-koa/config.js
@@ -0,0 +1,20 @@
+
+config.baseTitle = 'PHP异步编程: 手把手教你实现co与Koa'
+config.siteName = 'PHP异步编程: 手把手教你实现co与Koa'
+config.siteDes = 'PHP异步编程: 手把手教你实现co与Koa'
+config.siteKey = 'php-co-koa'
+config.assetBasePath = '/'
+
+// https://github.com/youzan/php-co-koa
+config.docProject = 'youzan/php-co-koa'
+config.project = 'youzan/php-co-koa'
+
+config.authorPage = 'https://github.com/youzan'
+config.authorName = 'youzan'
+
+config.theme = 'paper'
+config.codeTheme = 'github'
+config.catelogPage = 'SUMMARY.md'
+config.defaultPage = 'README.md'
+config.makeTOC = true
+config.emptyData = 'No content to display!'
