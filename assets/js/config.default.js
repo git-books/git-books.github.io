e89e705649f558ec328febb5adc9fe89dc56da9d
inhere <in.798@qq.com>
2017-10-09 09:52:48 +0800

    bug fixed for parse dataUrl and editUrl

--- assets/js/config.default.js
+++ assets/js/config.default.js
@@ -1,4 +1,4 @@
-
+// the all config
 const config = {
   // use for cache prefix
   siteName: 'beego doc',
@@ -8,6 +8,8 @@ const config = {
   baseTitle: 'beego-doc',
   assetBasePath: '',
   logoUrl: location.pathname,
+
+  docType: 'md', // 'rst'
 
   lang: '', // zh-CN
   langs: [], // ["en", "zh-CN"],
@@ -40,8 +42,8 @@ const config = {
   /* £ $ & β ξ ψ ℘ § */
   anchorIcon: '℘',
   tableClass: 'table table-bordered table-striped',
-  topLinks: ['<a href="/"><i class="fa fa-undo" aria-hidden="true"></i> back books list</a>'],
-  // topLinks: null,
+  // topLinks: ['<a href="/"><i class="fa fa-undo" aria-hidden="true"></i> back books list</a>'],
+  topLinks: null,
   bottomLinks: null,
 
   // some events
