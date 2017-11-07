aee00044b10c374d5756cc486afe3863891c8b80
inhere <in.798@qq.com>
2017-11-07 22:45:43 +0800

    add new book gorm

--- books.js
+++ books.js
@@ -18,6 +18,10 @@ const books = [{
   cover: 'https://raw.githubusercontent.com/songleo/the-little-go-book_ZH_CN/master/images/cover.jpg',
   description: '当开始学习一门新的语言时，我总有一种既爱又恨的感觉。一方面，语言是我们做事情的基础，即使是小的变化，也会带来明显的效果。当一些事情恍然大悟时，会给如何编程带来持久的影响，并且能够重新定义你关于其他语言的期望'
 }, {
+  url: '/books/gorm/',
+  name: 'gorm 文档',
+  description: 'The fantastic ORM library for Golang, aims to be developer friendly.'
+}, {
   url: '/books/google-grpc/',
   name: 'google gRpc 开发文档',
   description: 'google gRpc developer documentation'
---
+++ books/gorm/config.js
@@ -0,0 +1,21 @@
+  // page title = baseTile + content title
+config.baseTitle = 'Gorm'
+config.siteName = 'Gorm'
+config.siteDes = 'The fantastic ORM library for Golang, aims to be developer friendly.'
+config.siteKey = 'jinzhu-gorm'
+config.assetBasePath = '/'
+
+config.docProject = 'jinzhu/gorm'
+config.project = 'jinzhu/gorm'
+config.dataUrl = 'https://raw.githubusercontent.com/{docProject}/gh-pages/documents/'
+config.editUrl = 'https://github.com/{docProject}/blob/gh-pages/documents/'
+
+config.authorPage = 'https://github.com/jinzhu'
+config.authorName = 'jinzhu'
+
+config.theme = 'paper'
+config.codeTheme = 'github'
+config.catelogPage = 'SUMMARY.md'
+config.defaultPage = 'README.md'
+config.makeTOC = true
+config.emptyData = 'No content to display!'
